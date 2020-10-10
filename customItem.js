6bce5f5b34b5bccdc1f6791e20a8ab063c3ed430
Ro0t <62615149+WazeyDev@users.noreply.github.com>
2020-10-10 11:49:32 +0200

    Add files via upload

---
+++ customItem.js
@@ -0,0 +1,10 @@
+var itemId = 2001;
+var itemName = "Cocaine";
+
+
+ModPE.setItem(itemId, "images/cocaine.png", 0, itemName, 99);
+Item.setCategory(ItemCategory.INTERNAL)
+
+function useItem(x, y, z, item, block, side){
+  addItemInventory(itemId, 1, 0);
+}
